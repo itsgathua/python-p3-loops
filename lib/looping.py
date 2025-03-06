5f8a7143581b9a8be12a3196602575bc3fe3b753
Ian Gathua <gathuambui@gmail.com>
2025-03-06 14:26:56 +0300

    Final Commit

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    counter = 10
+    while counter > 0:
+        print(counter)
+        counter -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_list = []
+    for int in int_list:
+        int_list = int ** 2
+        squared_list.append(int_list)
+    return squared_list
 
 def fizzbuzz():
-    # code goes here!
-    pass
+        counter = 1
+        while counter < 101:
+            if counter % 3 == 0 and counter % 5 == 0:
+                print ("FizzBuzz")
+            elif counter % 3 == 0:
+                print ("Fizz")
+            elif counter % 5 == 0:
+                print ("Buzz")
+            else:
+                print (counter)
+            counter += 1
